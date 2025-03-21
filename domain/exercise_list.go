a1c4271ce54a52560fcc924bfba82c0698723958
Fauzan Rizky Ramadhan <fauzanrizky821@gmail.com>
2025-03-21 23:13:48 +0700

    add: change model exercise_list.go, log.go, schedule.go

--- domain/exercise_list.go
+++ domain/exercise_list.go
@@ -3,4 +3,6 @@ package domain
 type ExerciseList struct {
 	ScheduleID uint `gorm:"primaryKey"`
 	ExerciseID uint `gorm:"primaryKey"`
+	Set        int  `gorm:"not null"`
+	Repetition int  `gorm:"not null"`
 }
